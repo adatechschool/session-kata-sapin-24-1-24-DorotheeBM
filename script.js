5704bbff98e7276e446e4ec5158d97ad5d82fc07
DorotheeBM <braud.dorothee@gmail.com>
2025-01-24 11:41:55 +0100

    Create script.js
    première fonction afficherEtoiles()

---
+++ script.js
@@ -0,0 +1,14 @@
+// Détails de l’étape 1
+// 1.1 Une ligne d’étoiles
+// Commençons par écrire une fonction afficherEtoiles(n) qui affiche une ligne de * en fonction du paramètre n
+
+function afficherEtoiles(n) { // fonction pour afficher les étoiles
+    let etoiles = '*' // création de la première étoile
+    for (let index = 1; index < n; index++) { // pour chacune des n étoiles et initialisé à 1 car j'ai déjà créé une étoile au dessus
+        etoiles += '*' //pour ajouter les étoiles sur une ligne
+    }
+    console.log(etoiles) //console log pour faire apparaitre les étoiles
+}
+
+afficherEtoiles(2)
+afficherEtoiles(5)
